2ad0e27b48b46264c794ffa97bba162ad4b8ef19
uthamprabhu <uthamprabhu1@gmail.com>
2024-12-02 01:04:37 +0530

    welcome page impl till button

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,57 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from 'expo-router'
+import Swiper from 'react-native-swiper'
+import { onboarding } from '@/constants'
+
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null)
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  return (
+    <SafeAreaView className='flex h-full items-center justify-between bg-white'>
+      <TouchableOpacity
+        onPress={() => {
+          router.replace('/(auth)/sign-up')
+        }}
+        className='w-full flex justify-end items-end p-5'
+      >
+        <Text className='text-black text-md font-JakartaBold'>Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className='w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full' />
+        }
+        activeDot={
+          <View className='w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full' />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View
+            key={item.id}
+            className='flex items-center justify-center p-5'>
+            <Image
+              source={item.image}
+              className='w-full h-[300px]'
+              resizeMode='contain'
+            />
+            <View className='flex flex-row items-center justify-center w-full mt-10'>
+              <Text className='text-black text-3xl font-bold mx-10'>
+                {item.title}
+              </Text>
+            </View>
+
+            <Text className='text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3'>{item.description}</Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  )
+}
+
+export default Onboarding
